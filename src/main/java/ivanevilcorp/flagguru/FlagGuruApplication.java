78f15c90a8b014201cfffca6a06ca85fa75e1571
chrome heart <sengvihss@gmail.com>
2025-03-17 20:44:28 +0100

    add basic code to git

---
+++ src/main/java/ivanevilcorp/flagguru/FlagGuruApplication.java
@@ -0,0 +1,13 @@
+package ivanevilcorp.flagguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlagGuruApplication.class, args);
+    }
+
+}
