505ceb6abde3d5a5af0953a70dc8fcc5778b8710
chrome heart <sengvihss@gmail.com>
2025-03-17 21:17:18 +0100

    add new func with .env file

--- src/main/java/ivanevilcorp/flagguru/FlagGuruApplication.java
+++ src/main/java/ivanevilcorp/flagguru/FlagGuruApplication.java
@@ -1,5 +1,6 @@
 package ivanevilcorp.flagguru;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlagGuruApplication {
 
     public static void main(String[] args) {
+
+        // setting system variables from .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
         SpringApplication.run(FlagGuruApplication.class, args);
     }
 
